aefa85959b3c23615658037f4f12c24724cc6c29
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2023-12-06 18:16:27 +0100

    Add firebase to the project.

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0IEFk5SeVVJNBvympApemzMmFJfsw9UM",
+  authDomain: "fir-test-field.firebaseapp.com",
+  projectId: "fir-test-field",
+  storageBucket: "fir-test-field.appspot.com",
+  messagingSenderId: "1087960925332",
+  appId: "1:1087960925332:web:b22890ff708bb5e2392081",
+  measurementId: "G-0WK1E4Y30X",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
